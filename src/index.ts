5751716bb157967a7e9f58ea3d07114cd96fafa9
PsychedelicMonkey <ryan.getz.96@gmail.com>
2022-07-07 18:12:28 -0600

    Apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,45 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer, gql } from 'apollo-server-core';
 import express, { Application } from 'express';
+import { createServer } from 'http';
 
 import logger from './config/logger';
 
-const app: Application = express();
-
 const port = process.env.PORT || 5000;
 
-app.listen(port, () =>
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => {
+      return 'hello';
+    },
+  },
+};
+
+(async () => {
+  const app: Application = express();
+  const httpServer = createServer(app);
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    cache: 'bounded',
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise<void>((r) => httpServer.listen({ port }, r));
   logger.info({
     label: 'Application',
-    message: `Server running on port ${port}`,
-  })
-);
+    message: `🚀 Server ready in ${process.env.NODE_ENV} mode at http://localhost:${port}${server.graphqlPath}`,
+  });
+})();
