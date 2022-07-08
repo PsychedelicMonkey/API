f800335fecc9870368851bf2cbe1b1e3e704785f
PsychedelicMonkey <ryan.getz.96@gmail.com>
2022-07-07 18:38:01 -0600

    Login and registration mutations

--- src/graphql/user.ts
+++ src/graphql/user.ts
@@ -1,4 +1,4 @@
-import { gql } from 'apollo-server-express';
+import { AuthenticationError, gql } from 'apollo-server-express';
 import User from '../models/User';
 
 export const typeDefs = gql`
@@ -10,6 +10,11 @@ export const typeDefs = gql`
   type Query {
     user(id: ID!): User!
     users: [User]!
+  }
+
+  type Mutation {
+    loginUser(username: String!, password: String!): User!
+    registerUser(username: String!, email: String!, password: String!): User!
   }
 `;
 
@@ -24,6 +29,34 @@ export const resolvers = {
     users: async () => {
       const users = await User.find();
       return users;
+    },
+  },
+
+  Mutation: {
+    loginUser: async (parent: any, args: any) => {
+      const { username, password } = args;
+
+      const user = await User.findOne({ username }).select('+password').exec();
+
+      if (!user) {
+        throw new AuthenticationError('incorrect credentials');
+      }
+
+      const success = await user.checkPassword(password);
+
+      if (!success) {
+        throw new AuthenticationError('incorrect credentials');
+      }
+
+      return user;
+    },
+
+    registerUser: async (parent: any, args: any) => {
+      const { username, email, password } = args;
+
+      const user = await User.create({ username, email, password });
+
+      return user;
     },
   },
 };
